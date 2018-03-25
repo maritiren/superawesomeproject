5c21dbdc983d5d5ca87e038fbe393ca4cbdc7e0d
Marit Iren <marit.iren.92@gmail.com>
2018-03-25 22:11:33 +0200

    Move secret to local file.

--- src/config/constants.js
+++ src/config/constants.js
@@ -8,7 +8,6 @@ module.exports.Categories = {
     Misc: 'Miscellaneous'
 }
 
-module.exports.jwtSecret = '55CCE41AF215358AE42C75745989BAFE33E48100791DG2F1F8BE3FA5654102AE';
 module.exports.issuerDomain = 'api.superawesome.tghack.no';
 
 module.exports.scopes = {
